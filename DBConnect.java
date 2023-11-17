388c3a68619f30dc53f14082cf9e267e0efa9251
Matteo McCaughey SE2021 <matteo.mccaughey.2021@uni.strath.ac.uk>
2023-11-17 12:47:03 +0000

    Created Database connection function

--- DBConnect.java
+++ DBConnect.java
@@ -1,4 +1,24 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
 public class DBConnect {
+    private Connection con;
+    private Statement st;
+
+
+    public DBConnect() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/DBNAME", "USERNAME", "PASSWORD");
+            st = con.createStatement();
+        } catch (Exception ex) {
+            System.out.println("Error: " + ex);
+        }
+    }
+
+    
+
     public String[] read(String statement){
         //Apply SELECT statement to Database. (Could construct here, or individually when read() is being called.)
         //Read into array EITHER several fields of one record, or one field of several records.
