df355cb5a03ef4adffd035c3e69ab586cd2bdef7
Matteo McCaughey SE2021 <matteo.mccaughey.2021@uni.strath.ac.uk>
2023-11-21 01:11:38 +0000

    Implemented basic read and write functions.

--- DBConnect.java
+++ DBConnect.java
@@ -40,12 +40,42 @@ public class DBConnect {
         //Apply SELECT statement to Database. (Could construct here, or individually when read() is being called.)
         //Read into array EITHER several fields of one record, or one field of several records.
         //!!Catch SQL errors here. Return 1 in instance of error, 0 for instance of no results!!
+        if(mysqlConn != null){
+            try{
+                Statement stmt = mysqlConn.createStatement();
+                ResultSet rs = stmt.executeQuery(statement);
+                ResultSetMetaData rsmd = rs.getMetaData();
+                int columnsNumber = rsmd.getColumnCount();
+                String[] results = new String[columnsNumber];
+                while(rs.next()){
+                    for(int i=1; i<=columnsNumber; i++){
+                        results[i-1] = rs.getString(i);
+                    }
+                }
+                return results;
+            }
+            catch(SQLException e){
+                e.printStackTrace();
+                return null;
+            }
+        }
         return null;
     } 
 
     public int write(String statement){
         //As above, apply INSERT or UPDATE statement to Databse.
         //!!Catch SQL errors here. Return 1 in instance of error!!
-        return 0;
+        if(mysqlConn != null){
+            try{
+                Statement stmt = mysqlConn.createStatement();
+                stmt.executeUpdate(statement);
+                return 0;
+            }
+            catch(SQLException e){
+                e.printStackTrace();
+                return 1;
+        }    
+        }
+        return 1;
     }
 }
